e4eec19472ba03a9a34ce97b349a687b2240767f
happyc0der <Keshav.Rajput4@gmail.com>
2026-09-26 15:03:10 -0400

    Revert "CONTROL: the site's CSP allows inline script -- browser_e2e must fail (revert follows)"
    This reverts commit 7d37985f9b901a73d9a50921924e39699a6fcb38.

--- examples/site-node/server.mjs
+++ examples/site-node/server.mjs
@@ -44,7 +44,7 @@ const REPO = resolve(HERE, '..', '..');
 
 export const CSP = [
   "default-src 'none'",
-  "script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline'",   // CONTROL: CI must go red
+  "script-src 'self' 'wasm-unsafe-eval'",
   "connect-src 'self'",
   "style-src 'self'",
   "img-src 'self'",
